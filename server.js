a9851fae138ad21805a378ed42476cabbbaa989f
Tuan Phan Nguyen <tmp346@drexel.edu>
2019-04-03 21:36:30 -0400

    update ajax request to get data back from server

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const myParser = require("body-parser");
 const INDEX = path.join(__dirname, 'index.html');
-server = express()
-server.use((req, res) => res.sendFile(INDEX)).listen(PORT, () => console.log(`Listening on ${ PORT}`));
+server = express();
+server.use(myParser.urlencoded({extended : true}));
+
+var url = require('url');
+
+server.post('/endpoint', function(req, res){
+  console.log(req.body)
+  res.contentType('json');
+  res.send({ msg: JSON.stringify({response:'IT WORK'}) });
+
+
+});
+server.get('/', function(req,res){
+  res.sendFile(INDEX)
+})
+server.listen(PORT, () => console.log(`Listening on ${ PORT}`));
