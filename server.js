1d0906f06cd36fcb75b2c7b7d740a39f98e545b9
Tuan Phan Nguyen <tmp346@drexel.edu>
2019-04-04 21:29:38 -0400

    allow server to choose random move and get back to client

--- server.js
+++ server.js
@@ -3,17 +3,24 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 const myParser = require("body-parser");
 const INDEX = path.join(__dirname, 'index.html');
+var Chess = require('chess.js').Chess;
+
 server = express();
 server.use(myParser.urlencoded({extended : true}));
 
 var url = require('url');
-
+function get_move(game){
+  var possibleMoves = game.moves();
+  // game over
+  if (possibleMoves.length === 0) return;
+  var randomIndex = Math.floor(Math.random() * possibleMoves.length);
+  return possibleMoves[randomIndex]
+}
 server.post('/endpoint', function(req, res){
-  console.log(req.body)
+  game_state = Chess(req.body.state)
+  move = get_move(game_state);
   res.contentType('json');
-  res.send({ msg: JSON.stringify({response:'IT WORK'}) });
-
-
+  res.send({next_move: move });
 });
 server.get('/', function(req,res){
   res.sendFile(INDEX)
