975198df4465d85113c9ab910bf0d39fe49cbd35
Tuan Phan Nguyen <tmp346@drexel.edu>
2019-04-10 20:10:58 -0400

    simple max evaluation step for AI agent

--- server.js
+++ server.js
@@ -9,12 +9,43 @@ server = express();
 server.use(myParser.urlencoded({extended : true}));
 
 var url = require('url');
+var evaluateBoard = function (board) {
+    piece_value = {'p': 10, 'r': 50, 'n': 30, 'b': 30, 'q':90, 'k':900};
+    var totalEvaluation = 0;
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+          value = 0
+          if (board[i][j] != null){
+            value = piece_value[board[i][j].type]
+            if(board[i][j].color = 'w'){
+              value = -value;
+            }
+          }
+          totalEvaluation = totalEvaluation + value;
+        }
+    }
+    return totalEvaluation;
+};
 function get_move(game){
   var possibleMoves = game.moves();
+  var bestMove = null;
+    //use any negative large number
+  var bestValue = -9999;
+
+  for (var i = 0; i < possibleMoves.length; i++) {
+        var tmp_move = possibleMoves[i];
+        game.move(tmp_move);
+        //take the negative as AI plays as black
+        var boardValue = evaluateBoard(game.board())
+        game.undo();
+        if (boardValue > bestValue) {
+            bestValue = boardValue;
+            bestMove = tmp_move;
+        }
+  }
   // game over
   if (possibleMoves.length === 0) return;
-  var randomIndex = Math.floor(Math.random() * possibleMoves.length);
-  return possibleMoves[randomIndex]
+  return bestMove
 }
 server.post('/endpoint', function(req, res){
   game_state = Chess(req.body.state)
