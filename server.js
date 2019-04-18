25b0bb6f335995d21d306fdb9e571f695abe1eaa
Tuan Phan Nguyen <tmp346@drexel.edu>
2019-04-17 20:02:21 -0400

    add alpha-beta pruning algorithm to make the minmax search more efficient

--- server.js
+++ server.js
@@ -9,40 +9,71 @@ server = express();
 server.use(myParser.urlencoded({extended : true}));
 
 var url = require('url');
+var get_value_piece = function(piece, enemy){
+  if(piece == null) return 0;
+  value = 0
+  switch(piece.type){
+    case 'p':
+      value = 10;
+      break;
+    case 'n':
+      value = 30;
+      break;
+    case 'b':
+      value = 30;
+      break;
+    case 'r':
+      value = 50;
+      break;
+    case 'q':
+      value = 90;
+      break;
+    case 'k':
+      value = 900;
+      break;
+  }
+  if(piece.color == enemy) value = -value;
+  return value;
+}
 var evaluateBoard = function (board, enemy = 'w') {
-    piece_value = {'p': 10, 'r': 50, 'n': 30, 'b': 30, 'q':90, 'k':900};
     var totalEvaluation = 0;
     for (var i = 0; i < 8; i++) {
         for (var j = 0; j < 8; j++) {
-          value = 0
-          if (board[i][j] != null){
-            value = piece_value[board[i][j].type]
-            if(board[i][j].color == enemy){
-              value = -value;
-            }
-          }
-          totalEvaluation += value;
+          totalEvaluation += get_value_piece(board[i][j], enemy);
         }
     }
     return totalEvaluation;
 };
-var minmax = function(game, depth, isMin){
+var minmax = function(game, depth, alpha, beta, isMin){
   if (depth == 0) return evaluateBoard(game.board());
   var possibleMoves = game.moves();
   var best_score = -9999
-  if(isMin == true) best_score = 9999
-  for (var i = 0; i < possibleMoves.length; i++) {
-        var tmp_move = possibleMoves[i];
-        game.move(tmp_move);
-        //take the negative as AI plays as black
-        tmp_score = minmax(game, depth-1, !isMin)
-        game.undo();
-        if(isMin == true){
+  if(isMin == true){
+    best_score = 9999;
+    for (var i = 0; i < possibleMoves.length; i++) {
+          var tmp_move = possibleMoves[i];
+          game.move(tmp_move);
+          //take the negative as AI plays as black
+          tmp_score = minmax(game, depth-1, alpha, beta, !isMin)
+          game.undo();
           best_score = Math.min(best_score, tmp_score);
-        }
-        else{
+          beta = Math.min(best_score, beta);
+          if (alpha >= beta) break;
+    }
+  }
+  else{
+    best_score = -9999;
+    for (var i = 0; i < possibleMoves.length; i++) {
+          var tmp_move = possibleMoves[i];
+          game.move(tmp_move);
+          //take the negative as AI plays as black
+          tmp_score = minmax(game, depth-1, alpha, beta, !isMin)
+          game.undo();
           best_score = Math.max(best_score, tmp_score);
-        }
+          alpha = Math.max(best_score, alpha);
+
+          if (alpha >= beta) break;
+    }
   }
   return best_score;
 }
@@ -55,7 +86,7 @@ function get_move(game){
         var tmp_move = possibleMoves[i];
         game.move(tmp_move);
         //take the negative as AI plays as black
-        tmp_move_score = minmax(game, 2, true);
+        tmp_move_score = minmax(game, 2, -9999, 9999, true);
         game.undo();
         if (best_score <= tmp_move_score){
           best_move = tmp_move;
